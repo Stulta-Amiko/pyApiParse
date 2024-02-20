0c651ac33566f6a303b25514d428940f27744f58
Stulta-Amiko <phh01010312@gmail.com>
2024-02-20 22:17:29 +0900

    경로찾기 테스트

--- pyparse/busGraph.py
+++ pyparse/busGraph.py
@@ -1,44 +1,59 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 import csv
+import os
+
+if not (os.path.isdir('./data')):
+    os.makedirs('./data/graph')
+elif not(os.path.isdir('./data/graph')):
+    os.mkdir('./data/graph')
+
+if not(os.path.isfile('./data/graph/busGraph.edgelist')):
+    G = nx.Graph() 
+    f = open('./data/Express_Bus_Route_Detailed.csv','r')
+    rdr = csv.reader(f)
+
+    firstPass = 0
+
+    for line in rdr:
+
+        if firstPass == 0:
+            firstPass += 1
+        else:
+            G.add_edge(line[2],line[4],weight=int(line[5]),relation='express')  
 
 
-G = nx.Graph() 
-f = open('./data/Express_Bus_Route_Detailed.csv','r')
-rdr = csv.reader(f)
+    f = open('./data/Intercity_Bus_Route_Detailed.csv','r')
+    rdr = csv.reader(f)
+    
+    firstPass = 0
 
-firstPass = 0
+    for line in rdr:
 
-for line in rdr:
-
-    if firstPass == 0:
-        firstPass += 1
-    else:
-        G.add_edge(line[2],line[4],weight=int(line[5]),relation='express')  
-
-
-f = open('./data/Intercity_Bus_Route_Detailed.csv','r')
-rdr = csv.reader(f)
- 
-firstPass = 0
-
-for line in rdr:
-
-    if firstPass == 0:
-        firstPass += 1
-    else:
-        G.add_edge(line[2],line[4],weight=int(line[5]),relation='intercity')  
+        if firstPass == 0:
+            firstPass += 1
+        else:
+            G.add_edge(line[2],line[4],weight=int(line[5]),relation='intercity')  
 
 
-f.close()
+    f.close()
 
-#pos=nx.spring_layout(G)  각 노드, 엣지를 draw하기 위한 position 정보
-pos = nx.kamada_kawai_layout(G)
+    nx.write_weighted_edgelist(G,'./data/graph/busGraph.edgelist')
+else:
+    G = nx.read_weighted_edgelist('./data/graph/busGraph.edgelist')
+
 weight = nx.get_edge_attributes(G, 'weight')
 relation = nx.get_edge_attributes(G, 'relation')
 
+path = nx.astar_path(G, '춘천', '충주', heuristic=None, weight='weight')
+length = nx.astar_path_length(G,'춘천', '충주', heuristic=None, weight='weight')
+print("Path: ", path)
+print("Path length: ", length)
+
+'''
 nx.draw(G,pos,with_labels=True,font_family='AppleGothic', font_size=10)
 nx.draw_networkx_edge_labels(G,pos, edge_labels=weight)
+'''
 
 plt.show()
 
--- pyparse/testfield.py
+++ pyparse/testfield.py
@@ -1,18 +1,4 @@
-import csv
- 
-f = open('./data/Intercity_Bus_Route_Detailed.csv','r')
-rdr = csv.reader(f)
- 
-lines = []
+import os
 
-for line in rdr:
-    if line[5] == '5':
-        print(line[1]+','+line[3])
-'''
-f = open('./example.csv','w',newline='') #원본을 훼손할 위험이 있으니 다른 파일에 저장하는 것을 추천합니다.
-wr = csv.writer(f)
-wr.writerows(lines)
-'''
- 
-f.close()
- 
+if os.path.isfile('./ExpressbusRouteParse.py'):
+    print(os.path.isfile('./ExpressbusRouteParse1.py'))
